0ba56fc704add9aedae2cf15f3cf8c6d73671f34
d1iki1 <102245496+d1iki1@users.noreply.github.com>
2022-09-15 19:05:31 +0800

    Create sfz.php

---
+++ sfz.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>身份证验证</title>
+</head>
+<body>
+
+
+<form action="sfz2.php" method="post">
+名字: <input type="text" name="name">
+身份证号码: <input type="number" name="sfz">
+<input type="reset" value="清除">
+<input type="submit" value="提交"><br/>
+
+
+
+</form>	
+</body>
+</html>
